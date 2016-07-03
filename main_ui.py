1d5283f0b9ce2f92ec7f1224c4af1f850b1cadcd
Sedir Morais <philippi.sedir@gmail.com>
2016-07-03 19:49:25 -0300

    Movendo interface gráfica para o pacote correto

--- main_ui.py
+++ main_ui.py
@@ -5,21 +5,24 @@
 # Created by: PyQt5 UI code generator 5.6
 #
 # WARNING! All changes made in this file will be lost!
+
 import sys
+import locale
 
 from PyQt5 import uic
+
 
 if not getattr(sys, 'frozen', False):
     uic.compileUiDir('./ui')
 
-
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
+from PyQt5.QtCore import QLocale
 from ui.window_main_raw import Ui_MainWindow
 from ui.dialog_config import ConfigDialog
 
+
 # noinspection PyCallByClass
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super(MainWindow, self).__init__()
 
@@ -38,10 +41,12 @@ class MainWindow(QMainWindow):
         conf = ConfigDialog()
         conf.exec_()
 
+
 if __name__ == "__main__":
+    locale.setlocale(locale.LC_ALL, "pt_BR")
+    QLocale.setDefault(QLocale('pt_BR'))
+
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
-
-
